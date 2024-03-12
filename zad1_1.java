1a5948ae568b0e1a889b975f694805cda710797d
dejana šević <96137453+dejanaasevic@users.noreply.github.com>
2024-03-13 00:20:03 +0100

    Add files via upload

---
+++ zad1_1.java
@@ -0,0 +1,14 @@
+package vezbe_01;
+
+public class zad1_1 {
+
+	public static void main(String[] args) {
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("\t B.1");
+		System.out.println("\t\t B.1.1");
+		System.out.println("\t B.2");
+		System.out.println("C");
+		System.out.println();
+	}
+}
