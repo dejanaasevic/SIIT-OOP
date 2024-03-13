fb3db0a3c653a0b7310e72c7f830583577e7fee7
dejana šević <96137453+dejanaasevic@users.noreply.github.com>
2024-03-13 09:37:45 +0100

    Add files via upload

---
+++ zad1_2.java
@@ -0,0 +1,18 @@
+package vezbe_01;
+
+public class zad1_2 {
+
+	public static void main(String[] args) {
+		double povrsina_Kvadrata = 16;
+		double osnovica_Trougla = 4;
+		double krak_Trougla = 6;
+		double a = Math.sqrt(povrsina_Kvadrata);
+		double h = Math.sqrt(Math.pow(krak_Trougla, 2)- Math.pow(osnovica_Trougla, 2)/4);
+		double povrsina_Trougla = (osnovica_Trougla * h)/2;
+		
+		System.out.println("Duzina stranice kvadrata je: " + a);
+		System.out.println("Povrsina trougla je: " + povrsina_Trougla);
+
+	}
+
+}
