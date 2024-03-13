85cf21cd578543e92077037b21fb6c4fa6a1142d
dejana šević <96137453+dejanaasevic@users.noreply.github.com>
2024-03-13 10:18:00 +0100

    Add files via upload

---
+++ zad_1_5.java
@@ -0,0 +1,17 @@
+package vezbe_01;
+
+public class zad_1_5 {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 4;
+		int c = 5;
+		int povrsina_Kvadra = 2 * (a*b + a*c + b*c);
+		int zapremina_Kvadra = a*b*c;
+		
+		System.out.println("Povrsina kvadra je: " + povrsina_Kvadra);
+		System.out.println("Zapremina kvadra je: " + zapremina_Kvadra);
+
+	}
+
+}
