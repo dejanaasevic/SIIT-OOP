9bb8c7713d3324a63adcd8e784ccba7022ec2e7a
dejana šević <96137453+dejanaasevic@users.noreply.github.com>
2024-03-13 10:39:02 +0100

    Update zad_1_7.java

--- zad_1_7.java
+++ zad_1_7.java
@@ -3,7 +3,18 @@ package vezbe_01;
 public class zad_1_7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double x = 30;
+		double y = 20;
+		double max = Math.max(x, y);
+		double min = Math.min(x, y);
+		double z;
+		if (x<y) {
+			z = max/(1+Math.abs(min));
+		}
+		else {
+			z = max/(1+min);		
+		}
+		System.out.println(z);
 
 	}
 
