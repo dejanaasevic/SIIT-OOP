d3e637b9fa3c076feb348aba6958da3a091c0c59
dejana šević <96137453+dejanaasevic@users.noreply.github.com>
2024-03-14 09:58:04 +0100

    Add files via upload

---
+++ zad_2_4.java
@@ -0,0 +1,13 @@
+package vezbe_02;
+
+public class zad_2_4 {
+
+	public static void main(String[] args) {
+		int sum = 1;
+		for (int i = 10; i<=20; i+=2) {
+			sum*=i;
+		}
+		System.out.println("Suma je: " + sum);
+	}
+
+}
